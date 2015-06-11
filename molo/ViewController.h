02dab25c143a46097cecd0f2d781f53e17cef218
Geoffrey <gp335@cornell.edu>
2015-06-11 13:46:56 -0400

    Initial Commit

---
+++ molo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  molo
+//
+//  Created by Geoffrey Peterson on 6/11/15.
+//  Copyright (c) 2015 GLP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
