36e4bc2cfd34faff430308f82b6e8a46b56a725a
Geoffrey <gp335@cornell.edu>
2015-06-11 13:46:56 -0400

    Initial Commit

---
+++ molo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  molo
+//
+//  Created by Geoffrey Peterson on 6/11/15.
+//  Copyright (c) 2015 GLP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
